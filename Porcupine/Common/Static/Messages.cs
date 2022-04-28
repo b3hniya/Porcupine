3dd02a6dccda5087be1cdcdb8d101560e3855c30
b3hniya <b3hniya@gmail.com>
2022-04-28 19:33:14 +0430

    add Pipe Server Initialize message to the Messages.cs

--- Porcupine/Common/Static/Messages.cs
+++ Porcupine/Common/Static/Messages.cs
@@ -3,4 +3,5 @@ namespace Porcupine.Common.Static;
 public static class Messages
 {
     public static string PipeClientInitMessage = "Attempting to connect to the server...";
+    public static string PipeServerInitMessage = "Waiting for connection.";
 }
